bbf714bc249c684b8379cede3cfc96035786c89c
Logiase Song <logiase.song@gmail.com>
2024-12-06 02:41:43 +0800

    Create rust project

---
+++ README.md
@@ -0,0 +1,11 @@
+# MCX PAC
+
+Peripheral Access Crate (PAC) for NXP MCX Series MCUs
+
+This project is generated with [chiptool](https://github.com/embassy-rs/chiptool)
+
+## LICENSE
+
+This project is licensed under MIT LICENSE.
+
+The files from NXP are licensed under BSD-3 LICENSE.
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+// #![no_std]
+
+// use cfg_if::cfg_if;
+
+// mod common;
+
+// cfg_if! {
+//     if #[cfg(not(feature = "_device_selected"))] {
+//         compile_error!("no device feature selected");
+//     }
+// }
+
+// cfg_if! {
+//     if #[cfg(feature = "mcxn947")] {
+//         #[path = "mcxn947/chip.rs"]
+//         mod device;
+//     }
+// }
+
+// pub use device::*;
+
+#![no_std]
+#![allow(non_camel_case_types)]
+
+mod common;
+
+#[cfg_attr(feature = "mcxn947", path = "mcxn947/chip.rs")]
+mod device;
+
+#[cfg(feature = "_device_selected")]
+pub use device::*;
