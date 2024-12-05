91c649dcb043fdf9348f80760e04f440019cdde6
Logiase Song <logiase.song@gmail.com>
2024-12-06 02:53:37 +0800

    Update for publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,3 @@
-// #![no_std]
-
-// use cfg_if::cfg_if;
-
-// mod common;
-
-// cfg_if! {
-//     if #[cfg(not(feature = "_device_selected"))] {
-//         compile_error!("no device feature selected");
-//     }
-// }
-
-// cfg_if! {
-//     if #[cfg(feature = "mcxn947")] {
-//         #[path = "mcxn947/chip.rs"]
-//         mod device;
-//     }
-// }
-
-// pub use device::*;
-
 #![no_std]
 #![allow(unused_attributes)]
 #![allow(non_camel_case_types)]
