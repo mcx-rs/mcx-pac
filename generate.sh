c529e94424eac7c2af24cb616153a19286e64348
Logiase Song <logiase.song@nxp.com>
2024-12-16 11:43:15 +0800

    Merge all tasks into generate.sh.

--- generate.sh
+++ generate.sh
@@ -1,19 +1,48 @@
 #!/usr/bin/env bash
+set -e
 
-svdtools=svdtools
+CHIPTOOL_REV=d5ec99b8016e9e0f292d69fd14dcf9f00ec4e057
 
-if ! type "$svdtools" >/dev/null 2>/dev/null; then
-    cargo install --root .cargo-root svdtools
-    svdtools=./.cargo-root/bin/svdtools
-fi
+top_dir="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+crate_dir="${top_dir%%/}/output"
+crate_src_dir="${crate_dir%%/}/src"
 
-for i in svds/MCX*.yaml; do
-    echo "Patchingg $i"
-    $svdtools patch $i
+cargo install --root .cargo-root --git https://github.com/embassy-rs/chiptool.git --rev $CHIPTOOL_REV
+chiptool="${top_dir%%/}/.cargo-root/bin/chiptool"
+
+echo "Generating sources"
+for i in svds/*.svd; do
+    device=`basename -- $i`
+    device=${device%.*}
+    crate_device_dir="${crate_src_dir%%/}/${device,,}"
+
+    echo "  $device"
+
+    mkdir -p $crate_device_dir
+    cd $crate_device_dir
+    $chiptool generate --svd "${top_dir%%/}/svds/$device.svd" --transform "${top_dir%%/}/transforms/$device.yaml"
 done
+cd $top_dir
 
-echo "Generating crate"
-RUST_LOG=mcx_pac_generator=debug cargo run --release -- crate
-
-echo "Formatting output crate"
-cargo fmt --manifest-path output/Cargo.toml
+echo "Copying Cargo.toml"
+if version=`git describe 2>/dev/null`; [ $? -ne 0 ]; then
+    version="0.0.0-nightly"
+fi
+sed "s/__VERSION__/$version/" src/Cargo.toml > "${crate_dir%%/}/Cargo.toml"
+echo "Copying build.rs"
+cp src/build.rs $crate_dir
+echo "Copying common.rs"
+cp src/common.rs $crate_src_dir
+echo "Copying lib.rs"
+cp src/lib.rs $crate_src_dir
+for i in svds/*.svd; do
+    device=`basename -- $i`
+    device=${device%。*}
+    device=${device,,}
+    echo "#[cfg_attr(feature = \"$device\", path = \"$device/lib.rs\")]" >> ${crate_src_dir%%/}/lib.rs
+done
+echo "mod device;" >> ${crate_src_dir%%/}/lib.rs
+echo "#[cfg(feature = \"_device_selected\")]" >> ${crate_src_dir%%/}/lib.rs
+echo "pub use device::*;" >> ${crate_src_dir%%/}/lib.rs
+touch ${crate_src_dir%%/}/device.rs
+cp .gitignore $crate_dir
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![no_std]
+#![allow(dead_code)]
+#![allow(unused_attributes)]
+#![allow(non_camel_case_types)]
+
+mod common;
+
